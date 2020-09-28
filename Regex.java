9509c53b0a27b2ad3b9dfd42340065815d9e622f
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-28 16:28:34 +0530

    [Jitendra] Added : Validate mail with all required parts

--- Regex.java
+++ Regex.java
@@ -8,7 +8,14 @@ public class Regex{
 	Scanner sc=new Scanner(System.in);
 	String mail=sc.nextLine();
 	
-	Pattern pattern = Pattern.compile("^[_A-Za-z0-9+-]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$"); //Email Validation
+	Pattern pattern = Pattern.compile("^[_A-Za-z0-9+-]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"); //Email Validation
+	
+	//[_A-Za-z0-9+-]+                     ----> Matches Uppercase or Lowercase or digits with underscore one or more time before @
+	//(\\.[_A-Za-z0-9-]+)* 		      ---> Matches Uppercase or Lowercase or digits with underscore zero or more time (If dot is present) before @
+	//[A-Za-z0-9-]+               	      --->Matches Uppercase or Lowercase or digits one or more time after @
+	//(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,}) ---> Matches Uppercase or Lowercase or digits with underscore zero or more time followed by after dot Uppercase or Lowercase with the length of minimum 2 after @
+	
+
 	Matcher m=pattern.matcher(mail);                                       
 	int i=0;
 
